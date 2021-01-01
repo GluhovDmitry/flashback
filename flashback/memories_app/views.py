f360fecd5e42b340fe746d9690e68e0196851b79
GluhovDmitry <you@example.com>
2021-01-01 17:42:49 +0500

    Added new entity for login form

--- flashback/memories_app/views.py
+++ flashback/memories_app/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 #def description(request):
 #	return HttpResponse("This service is managed to keep your memories...")
 
-def description(request):
+def index(request):
 	return render(request, 'index.html', {})
+@login_required
+def home(request):
+	return render(request, 'home.html', {})
+
+def login(request):
+	return render(request, 'login.html', {})
