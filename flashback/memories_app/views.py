fbe58d802445e3a8e0864acf5182d5312a426803
GluhovDmitry <you@example.com>
2021-01-01 16:14:16 +0500

    Added html template

--- flashback/memories_app/views.py
+++ flashback/memories_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#def description(request):
+#	return HttpResponse("This service is managed to keep your memories...")
+
 def description(request):
-	return HttpResponse("This service is managed to keep your memories...")
-
-
+	return render(request, 'index.html', {})
