666c627b0373f2cbb8553c11c04ca3fd2b4ae136
GluhovDmitry <you@example.com>
2021-01-08 01:38:41 +0500

    Created the model for memories posts and generate it into admin

--- flashback/memories_app/models.py
+++ flashback/memories_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	description =  models.TextField(max_length=70)
+	comment = models.TextField()
+	longitude = models.CharField(max_length=10)
+	latitude = models.CharField(max_length=10)
