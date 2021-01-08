4011e9175a179335fdbaaf305a655c7d543c9b24
GluhovDmitry <you@example.com>
2021-01-09 02:17:08 +0500

    Restricted memories on user's home page by author

--- flashback/memories_app/models.py
+++ flashback/memories_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner', blank = True, null = True)
 	description =  models.TextField(max_length=70)
 	comment = models.TextField()
 	#longitude = models.CharField(max_length=10)
