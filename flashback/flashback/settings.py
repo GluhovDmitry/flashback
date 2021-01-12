e33b76f4efa6c37a8bb8f1ed56369a92dcd7a524
GluhovDmitry <you@example.com>
2021-01-12 05:30:06 +0500

    deploy

--- flashback/flashback/settings.py
+++ flashback/flashback/settings.py
@@ -13,12 +13,12 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
