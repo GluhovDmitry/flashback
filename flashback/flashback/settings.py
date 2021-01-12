ed13612d5c176d76a5b9f5d4ad33fb386c4c818c
GluhovDmitry <you@example.com>
2021-01-12 06:41:35 +0500

    Changed path for wsgi and settings in wsgi

--- flashback/flashback/settings.py
+++ flashback/flashback/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'flashback.urls'
+ROOT_URLCONF = 'flashback.flashback.urls'
 
 TEMPLATES = [
     {
