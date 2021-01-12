5ead7e2bc3e2dc7a1fb16580aa551da31541a4e4
GluhovDmitry <you@example.com>
2021-01-12 05:15:16 +0500

    deploy

--- flashback/flashback/settings.py
+++ flashback/flashback/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
+import dj-database-url
 
 db_from_env = dj-database-url.config()
 DATABASE['default'].update(db_from_env)
