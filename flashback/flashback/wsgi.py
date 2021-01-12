72360859e74fb04e9daebf46f63b8636538d250c
GluhovDmitry <you@example.com>
2021-01-12 06:15:18 +0500

    Changed path for wsgi and settings in wsgi

--- flashback/flashback/wsgi.py
+++ flashback/flashback/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashback.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashback.flashback.settings')
 
 application = get_wsgi_application()
